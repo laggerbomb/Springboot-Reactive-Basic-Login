2396eaa3f47e9e9b37e6ca6073ac725b687fc12f
zloh <zloh@deloitte.com>
2023-12-19 13:07:05 +0800

    Remove unused import

--- springboot-basic-login/src/main/java/com/example/springbootbasiclogin/service/AuthService.java
+++ springboot-basic-login/src/main/java/com/example/springbootbasiclogin/service/AuthService.java
@@ -9,7 +9,6 @@ import com.example.springbootbasiclogin.repo.VerificationTokenRepository;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.core.context.SecurityContextHolder;
